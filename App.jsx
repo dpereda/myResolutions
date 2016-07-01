a012de4649ec47f78e8691d026acb4a466b88dde
Dan Pereda <dpereda@gmail.com>
2016-07-01 17:00:28 -0400

    Add with git-plus plugin

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+Resolutions = new Mongo.Collection("resolutions");
 
 export default class App extends React.Component {
   render() {
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@ Hello, This is my run at the LevelUpTuts tutorial on meteor and react.
 
 Now since react is now on v.15 there have had to be some changes.
 
- 
+ Lesson 3 Done
